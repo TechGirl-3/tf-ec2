d8abed7b7d0bc10284b215a0f2db5d5562698eed
root <root@ip-172-31-59-252.ec2.internal>
2020-08-14 07:32:45 +0000

    ssh key

--- EC2/test/ssh_test.go
+++ EC2/test/ssh_test.go
@@ -1,41 +1,28 @@
+
 package test
 
 import (
-        "testing"
-	//"strings"
-        "fmt"
-	"github.com/gruntwork-io/terratest/modules/aws"
-        "github.com/gruntwork-io/terratest/modules/terraform"
+	"testing"
+	"regexp"
+	"log"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTerraformAwsExample(t *testing.T) {
-        t.Parallel()
-        awsRegion := "us-east-1"
-        expectedKey := "gotest"
-        terraformOptions := &terraform.Options{
-                TerraformDir: "../examples",
-                // Environment variables to set when running Terraform
-                EnvVars: map[string]string{
-                        "AWS_DEFAULT_REGION": awsRegion,
-			"GOCACHE" : "off",
-                },
-        }
-        defer terraform.Destroy(t, terraformOptions)
-        terraform.InitAndApply(t, terraformOptions)
-        instanceKey := terraform.Output(t, terraformOptions, "instance-key")
-        //fmt.Printf("%s\n", instanceKey)
-	//instanceKey = strings.Replace(instanceKey, "\n", "", -1)
-	assert.Equal(t, expectedKey, instanceKey)
+func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples",
+	}
 
-	//Tags
-	instanceID := terraform.Output(t, terraformOptions, "instance_id")
-	//aws.AddTagsToResource(t, awsRegion, instanceID, map[string]string{"Name": "testing-tag-value"})
-	instanceTags := aws.GetTagsForEc2Instance(t, awsRegion, instanceID)
-	fmt.Printf("%s Tags \n", instanceTags)
-	
-	nameTag, containsNameTag := instanceTags["Name"]
-	assert.True(t, containsNameTag)
-	assert.Equal(t, "testtera", nameTag)
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "instance-key")
+	re, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	output = re.ReplaceAllString(output, "")
+	assert.Equal(t, "check", output)
 }
-
